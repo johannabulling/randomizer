4d225589dd724d7f42d49c053b2e56d14cab2bab
Max Stralka <maxstralka@me.com>
2014-09-11 12:35:38 +0200

    merging with Johanna

--- index.php
+++ index.php
@@ -34,7 +34,20 @@
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
           <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-certificate small-glyphicon"></span> Trendyfizer</a>
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
         </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="Wildcard/wildcard.html">WILDCARD</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
     </nav>
 
